6f2d62a5f2d994f7b6a834a7ab1527bca187cda9
Enea <eneasecondary@gmail.com>
2018-10-23 11:58:09 -0700

    Base site established - Work on api next

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark">
+    <a className="navbar-brand" href="/">
+      New York Times - Scraper
+    </a>
+  </nav>
+);
+
+export default Nav;
---
+++ client/src/pages/Articles/Articles.js
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+
+class Articles extends Component {
+  state = {
+    articles: [],
+    topic: "",
+    startYear: "",
+    endYear: ""
+  };
+
+  componentDidMount() {
+    this.loadArticles();
+  }
+
+  loadArticles = () => {
+    API.getArticles()
+      .then(res =>
+        this.setState({
+          articles: res.data,
+          topic: "",
+          startYear: "",
+          endYear: ""
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteArticle = id => {
+    API.deleteArticle(id)
+      .then(res => this.loadArticles())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.topic && this.state.startYear) {
+      API.saveArticle({
+        topic: this.state.topic,
+        startYear: this.state.startYear,
+        endYear: this.state.endYear
+      })
+        .then(res => this.loadArticles())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>What Articles are you Scraping?</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                value={this.state.topic}
+                onChange={this.handleInputChange}
+                name="topic"
+                placeholder="Topic (required)"
+              />
+              <Input
+                value={this.state.startYear}
+                onChange={this.handleInputChange}
+                name="startYear"
+                placeholder="Start Year (required)"
+              />
+              <Input
+                value={this.state.endYear}
+                onChange={this.handleInputChange}
+                name="endYear"
+                placeholder="End Year (required)"
+              />
+              <FormBtn
+                disabled={!(this.state.startYear && this.state.topic)}
+                onClick={this.handleFormSubmit}
+              >
+                Search
+              </FormBtn>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12 sm-12">
+            <Jumbotron>
+              <h1>Article List</h1>
+            </Jumbotron>
+            {this.state.articles.length ? (
+              <List>
+                {this.state.articles.map(article => (
+                  <ListItem key={article._id}>
+                    <Link to={"/articles/" + article._id}>
+                      <strong>
+                        {article.topic} by {article.startYear}
+                      </strong>
+                    </Link>
+                    <DeleteBtn
+                      onClick={() => this.deleteArticle(article._id)}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12 sm-12">
+            <Jumbotron>
+              <h1>Saved Articles</h1>
+            </Jumbotron>
+            {this.state.articles.length ? (
+              <List>
+                {this.state.articles.map(article => (
+                  <ListItem key={article._id}>
+                    <Link to={"/articles/" + article._id}>
+                      <strong>
+                        {article.topic} by {article.startYear}
+                      </strong>
+                    </Link>
+                    <DeleteBtn
+                      onClick={() => this.deleteArticle(article._id)}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Articles;
